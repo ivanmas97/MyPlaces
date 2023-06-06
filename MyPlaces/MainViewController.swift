c228770f33c42e2dba40be2bec39b0ad4d6e9316
ivanmas97 <ivanmas97@yandex.ru>
2023-06-06 18:11:09 +0300

    Delete Action

--- MyPlaces/MainViewController.swift
+++ MyPlaces/MainViewController.swift
@@ -42,6 +42,19 @@ class MainViewController: UITableViewController {
         return cell
     }
 
+    // MARK: - Table view delegate
+    
+    override func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        
+        let place = places[indexPath.row]
+        let deleteAction = UIContextualAction(style: .destructive, title: "Delete") { (_, _, _) in
+            
+            StorageManager.deleteObject(place)
+            tableView.deleteRows(at: [indexPath], with: .automatic)
+        }
+        return UISwipeActionsConfiguration(actions: [deleteAction])
+    }
+    
     /*
     // MARK: - Navigation
 
